6ae04d7bdb05a7bb4dd9412e5f8b24d38fd65942
mmichaelis <mmichaelis@ap2002.chtc.wisc.edu>
2025-06-16 15:51:17 -0500

    changed sh/sub names to 01_demux and updated script accordingly

---
+++ 01_demux.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+echo "Unpacking input files..."
+tar xzf emp-single-end-sequences.tar.gz
+
+echo "Running QIIME tools import..."
+qiime tools import \
+  --type 'EMPSingleEndSequences' \
+  --input-path emp-single-end-sequences \
+  --output-path emp-single-end-sequences.qza
+
+echo "Demultiplexing..."
+qiime demux emp-single \
+  --i-seqs emp-single-end-sequences.qza \
+  --m-barcodes-file sample-metadata.tsv \
+  --m-barcodes-column barcode-sequence \
+  --o-per-sample-sequences demux.qza \
+  --o-error-correction-details demux-details.qza \
+  --p-rev-comp-mapping-barcodes
+
+echo "Running QIIME summarize..."
+qiime demux summarize \
+  --i-data demux.qza \
+  --o-visualization demux.qzv
+
+echo "Step Complete!"
