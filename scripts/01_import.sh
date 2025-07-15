c20f8d1742fe610180ed94df8cc0aa4365060919
mmichaelis <mmichaelis@ap2002.chtc.wisc.edu>
2025-07-15 11:09:53 -0500

    edited input paths

--- scripts/01_import.sh
+++ scripts/01_import.sh
@@ -7,14 +7,24 @@ echo ${NETID}
 echo "Container launched successfully..."
 
 echo "Importing with QIIME tools import..."
+<<<<<<< HEAD
 qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' \
   --input-path  /staging/${NETID}/seqs
   --input-format CasavaOneEightSingleLanePerSampleDirFmt \
   --output-path demux.qza
+=======
+
+qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' \
+  --input-path  /staging/${NETID}/input_outputs/00_pipeline_inputs/seqs \
+  --input-format CasavaOneEightSingleLanePerSampleDirFmt \
+  --output-path demux.qza
+
+ls -lht
+>>>>>>> b797a17 (added correct input paths)
 
 echo "Running QIIME summarize..."
 qiime demux summarize \
   --i-data demux.qza \
   --o-visualization demux.qzv
 
-echo "Job 01 Complete!"
+ls -lht
