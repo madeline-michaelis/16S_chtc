2dd7b9091618ccb24ffe27339e2d4c10451241a3
Patricia Tran <ptran5@wisc.edu>
2025-07-15 20:07:14 -0500

    made the mkdir.sh script take arguments to automatically create folder; reorganized folder such that the metadata is in the 00_pipeline_inputs folder; tested with paired-end reads data; modified the make_dag.sh script to take in the project folder and the group that we want to plot step 05 and 08 by; improved instructions

--- scripts/01_import.sh
+++ scripts/01_import.sh
@@ -2,25 +2,19 @@
 
 set -e
 NETID="$1"
+PROJECT="$2"
 echo ${NETID}
+echo ${PROJECT}
 
 echo "Container launched successfully..."
 
 echo "Importing with QIIME tools import..."
-<<<<<<< HEAD
 qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' \
-  --input-path  /staging/${NETID}/seqs
-  --input-format CasavaOneEightSingleLanePerSampleDirFmt \
-  --output-path demux.qza
-=======
-
-qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' \
-  --input-path  /staging/${NETID}/input_outputs/00_pipeline_inputs/seqs \
+  --input-path  /staging/${NETID}/${PROJECT}/input_outputs/00_pipeline_inputs/seqs \
   --input-format CasavaOneEightSingleLanePerSampleDirFmt \
   --output-path demux.qza
 
 ls -lht
->>>>>>> b797a17 (added correct input paths)
 
 echo "Running QIIME summarize..."
 qiime demux summarize \
